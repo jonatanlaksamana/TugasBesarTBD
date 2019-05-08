df8d6f05c590bddac9b80514b2a513b5d329b78e
i16045 <reynaldiirfannn@gmail.com>
2019-05-08 11:46:02 +0700

    styling home

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -3,32 +3,32 @@
 <html>
 
 
-<body background="{{asset('/images/multicol.jpg')}}" >
+<body background="{{asset('/images/multicol.jpg')}}" style="width:98.67%">
 
-<div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-    <img src="{{asset('/images/crumpledpaper.jpg')}}" style="width:200px;height:200px; position:relative;"  >
+<div class="flip-card" style="border-radius:20px">
+  <div class="flip-card-inner" style="border-radius:20px">
+    <div class="flip-card-front" style="border-radius:20px">
+    <img src="{{asset('/images/crumpledpaper.jpg')}}" style="width:200px;height:200px; position:relative;border-radius:20px"  >
       <p style="position: absolute;">Pilih Jadwal</p>
       
     </div>
-    <div class="flip-card-back">
+    <div class="flip-card-back" style="border-radius:20px">
       
       <p>Memilih jadwal mengajar.</p>
     </div>
   </div>
 </div>
 
-<div class="flip-card" >
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
+<div class="flip-card" style="border-radius:20px">
+  <div class="flip-card-inner" style="border-radius:20px">
+    <div class="flip-card-front" style="border-radius:20px">
     
-      <img src="{{asset('/images/crumpledpaper.jpg')}}" style="width:200px;height:200px; position:relative;"  >
+      <img src="{{asset('/images/crumpledpaper.jpg')}}" style="width:200px;height:200px; position:relative;border-radius:20px"  >
     
       <p>Seluruh Jadwal</p>
       
     </div>
-    <div class="flip-card-back">
+    <div class="flip-card-back" style="border-radius:20px">
         <a href="{{url('jadwals/1')}}">
       <p>Menapilkan seluruh jadwal mengajar.</p>
     </a>
@@ -37,12 +37,12 @@
 </div>
 
 <div class="flip-card" >
-        <div class="flip-card-inner">
-          <div class="flip-card-front">
-          <img src="{{asset('/images/crumpledpaper.jpg')}}" style="width:200px;height:200px; position:relative;"  >
+        <div class="flip-card-inner" style="border-radius:20px">
+          <div class="flip-card-front" style="border-radius:20px">
+          <img src="{{asset('/images/crumpledpaper.jpg')}}" style="width:200px;height:200px; position:relative;border-radius:20px"  >
                 <p>Ganti Jadwal</p>
           </div>
-          <div class="flip-card-back">
+          <div class="flip-card-back" style="border-radius:20px">
            
             <p>Mengganti jadwal untuk kelas pengganti.</p>
           </div>
--- resources/views/layout/loginlayout.blade.php
+++ resources/views/layout/loginlayout.blade.php
@@ -4,11 +4,11 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('/css/bootstrap.min.css')}}">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('/css/animate.css')}}">
 <!--===============================================================================================-->	
